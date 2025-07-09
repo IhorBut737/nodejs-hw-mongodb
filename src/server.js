8aec79197b0b18fa46942cf2a98851faa79d1fdd
IhorBut737 <igor.klad@gmail.com>
2025-07-09 11:37:35 +0300

    commit auth

--- src/server.js
+++ src/server.js
@@ -3,9 +3,10 @@ import cors from 'cors';
 import pino from 'pino-http';
 
 import { getEnvVar } from './utils/getEnvVar.js';
-import contactsRouter from './routers/contacts.js';
+import router from './routers/index.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -14,6 +15,9 @@ export const setupServer = () => {
 
   app.use(express.json());
   app.use(cors());
+
+  app.use(cookieParser());
+
   app.use(pino());
 
   app.get('/', (req, res) => {
@@ -22,7 +26,7 @@ export const setupServer = () => {
     });
   });
 
-  app.use(contactsRouter);
+  app.use(router);
 
   app.use(notFoundHandler);
 
